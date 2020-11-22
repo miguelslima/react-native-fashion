482148baac69aac992f65bbdb667f37b0ada5c0c
Miguel <miguelsousalima@hotmail.com>
2020-11-22 11:01:46 -0300

    config routes

--- App.tsx
+++ App.tsx
@@ -1,21 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Onboarding from "./src/Authentication/Onboarding";
+
+const AuthenticationStack = createStackNavigator();
+
+const AuthenticationNavigation = () => {
+  return (
+    <AuthenticationStack.Navigator>
+      <AuthenticationStack.Screen name="Onboarding" component={Onboarding} />
+    </AuthenticationStack.Navigator>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <AuthenticationNavigation />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/Authentication/Onboarding/Onboarding.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View } from "react-native";
+
+// import { Container } from './styles';
+
+const Onboarding: React.FC = () => {
+  return <View style={{ backgroundColor: "#f00", flex: 1 }}></View>;
+};
+
+export default Onboarding;
